9e03388bd042f80a93bfd0cb58bddf617f6aa300
John Wonson <jmpwonson@gmail.com>
2020-12-23 10:59:21 +1100

    Create DummyEntities.cs
    Implementation update: ValidationEngine -> Tests -> Helpers -> DummyEntities.cs.

---
+++ CSharpValidatorEngine/CSharpValidatorEngine/ValidationEngine.Tests/Helpers/DummyEntities.cs
@@ -0,0 +1,15 @@
+﻿namespace ValidationEngine.Tests.Helpers
+{
+    internal class TestEntity
+    {
+        public SubEntity[] SubClassArray { get; set; }
+        public string[] StringArray { get; set; }
+        public SubEntity SubClass { get; set; }
+    }
+
+    internal class SubEntity
+    {
+        public string TestProp { get; set; }
+        public string[] SecondarySub { get; set; }
+    }
+}
