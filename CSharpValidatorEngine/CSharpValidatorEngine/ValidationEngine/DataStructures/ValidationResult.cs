3f44008eb37a1fa4eed945c0830689a35326d242
John Wonson <jmpwonson@gmail.com>
2020-12-23 10:19:26 +1100

    Create ValidationResult.cs
    Implementation update: ValidationEngine -> DataStructures -> ValidationResult.cs.

---
+++ CSharpValidatorEngine/CSharpValidatorEngine/ValidationEngine/DataStructures/ValidationResult.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+using ValidationEngine.Engine;
+
+namespace ValidationEngine.DataStructures
+{
+    public class ValidationResult
+    {
+        public bool Succeeded { get; set; }
+        public List<Rule> SucceededRules { get; set; }
+        public List<Rule> FailedRules { get; set; }
+        public List<Rule> NotRanRules { get; set; }
+    }
+}
