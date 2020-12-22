1e4fc23e498bae14523681d06733efcc91844b09
John Wonson <jmpwonson@gmail.com>
2020-12-23 09:13:20 +1100

    Create FieldComparison.cs
    Implementation update: ValidationEngine -> DataStructures -> FieldComparison.cs.

---
+++ CSharpValidatorEngine/CSharpValidatorEngine/ValidationEngine/DataStructures/FieldComparison.cs
@@ -0,0 +1,9 @@
+﻿namespace ValidationEngine.DataStructures
+{
+    public class FieldComparison
+    {
+        public int? ArrayIndex { get; set; }
+        public string FieldPath { get; set; }
+        public string FieldName { get; set; }
+    }
+}
