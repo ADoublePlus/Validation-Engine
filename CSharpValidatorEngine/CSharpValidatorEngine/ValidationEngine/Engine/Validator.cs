3f466dcb27faf94c675d500d14cd7f905299e110
John Wonson <jmpwonson@gmail.com>
2020-12-23 10:44:37 +1100

    Create Validator.cs
    Implementation update: ValidationEngine -> Engine -> Validator.cs.

---
+++ CSharpValidatorEngine/CSharpValidatorEngine/ValidationEngine/Engine/Validator.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+
+using ValidationEngine.DataStructures;
+
+namespace ValidationEngine.Engine
+{
+    public class Validator
+    {
+        public string Name { get; set; }
+        public List<RuleSet> RuleSets { get; set; }
+        
+        private List<Rule> SucceededRules { get; set; }
+        private List<Rule> FailedRules { get; set; }
+        private List<Rule> NotRanRules { get; set; }
+
+        public ValidationResult Execute(params object[] entities)
+        {
+            SucceededRules = new List<Rule>();
+            FailedRules = new List<Rule>();
+            NotRanRules = new List<Rule>();
+
+            bool succeed = false;
+
+            foreach (var ruleSet in RuleSets)
+            {
+                succeed = ruleSet.Run(entities);
+
+                SucceededRules.AddRange(ruleSet.SucceededRules);
+                FailedRules.AddRange(ruleSet.FailedRules);
+                NotRanRules.AddRange(ruleSet.NotRanRules);
+
+                if (!succeed)
+                    break;
+            }
+
+            return new ValidationResult()
+            {
+                FailedRules = this.FailedRules,
+                SucceededRules = this.SucceededRules,
+                NotRanRules = this.NotRanRules,
+
+                Succeeded = succeed
+            };
+        }
+    }
+}
