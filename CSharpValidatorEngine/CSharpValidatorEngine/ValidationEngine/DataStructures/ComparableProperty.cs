5e8ccbc445f6a89bddf018bfae96e670a6720961
John Wonson <jmpwonson@gmail.com>
2020-12-23 09:08:31 +1100

    Create ComparableProperty.cs
    Implementation update: ValidationEngine -> DataStructures -> ComparableProperty.cs.

---
+++ CSharpValidatorEngine/CSharpValidatorEngine/ValidationEngine/DataStructures/ComparableProperty.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Reflection;
+
+namespace ValidationEngine.DataStructures
+{
+    public class ComparableProperty
+    {
+        public object Entity { get; set; }
+        public PropertyInfo PropertyInfo { get; set; }
+
+        public string PropertyValue
+        {
+            get
+            {
+                if (this.PropertyInfo.PropertyType.IsArray && this._arrayIndex.HasValue)
+                {
+                    Array arr = (Array)this.PropertyInfo.GetValue(this.Entity, null);
+                    return (string)arr.GetValue(this._arrayIndex.Value);
+                }
+                else
+                    return (string)this.PropertyInfo.GetValue(this.Entity, null);
+            }
+        }
+
+        private int? _arrayIndex;
+
+        public ComparableProperty(object entity, PropertyInfo propertyInfo, int? arrayIndex)
+        {
+            this._arrayIndex = arrayIndex;
+            this.PropertyInfo = propertyInfo;
+            this.Entity = entity;
+        }
+    }
+}
