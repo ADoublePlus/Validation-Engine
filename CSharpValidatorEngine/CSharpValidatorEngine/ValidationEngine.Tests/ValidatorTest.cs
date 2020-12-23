d141c74ef59a9e7cb8ab9d7cc2f8fe037071b323
John Wonson <jmpwonson@gmail.com>
2020-12-23 12:43:51 +1100

    Create ValidatorTest.cs
    Implementation update: ValidationEngine -> Tests -> ValidatorTest.cs.

---
+++ CSharpValidatorEngine/CSharpValidatorEngine/ValidationEngine.Tests/ValidatorTest.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections.Generic;
+
+using ValidationEngine.DataStructures;
+using ValidationEngine.Engine;
+using ValidationEngine.Tests.Helpers;
+
+using NUnit.Framework;
+
+namespace ValidationEngine.Tests
+{
+    [TestFixture]
+    public class ValidatorTest
+    {
+        private Validator GetValidator()
+        {
+            return new Validator()
+            {
+                Name = "test validation",
+                RuleSets = new List<RuleSet>()
+                {
+                    new RuleSet()
+                    {
+                        OnInvalid = (prop) => {}, // Do nothing
+                        Rules = new List<Rule>()
+                        {
+                            new Rule()
+                            {
+                                Name = "Test rule",
+                                ValidationExpression = "^.*TEST.*$",
+                                FieldName = "TestProp",
+                                FieldPath = "TestEntity.SubClassArray",
+                                ArrayIndex = 0
+                            }
+                        }
+                    }
+                }
+            };
+        }
+
+        [Test]
+        public void Test_validate_succeed()
+        {
+            Validator v = GetValidator();
+
+            TestEntity t = new TestEntity()
+            {
+                SubClassArray = new SubEntity[]
+                {
+                    new SubEntity()
+                    {
+                        TestProp = "this is a TEST"
+                    }
+                }
+            };
+
+            ValidationResult validationResult = v.Execute(t);
+
+            Assert.IsTrue(validationResult.SucceededRules.Count == 1);
+        }
+
+        [Test]
+        public void Test_validate_fail()
+        {
+            Validator v = GetValidator();
+
+            TestEntity t = new TestEntity()
+            {
+                SubClassArray = new SubEntity[]
+                {
+                    new SubEntity()
+                    {
+                        TestProp = "this is not...."
+                    }
+                }
+            };
+
+            ValidationResult validationResult = v.Execute(t);
+
+            Assert.IsTrue(validationResult.FailedRules.Count == 1);
+        }
+    }
+}
